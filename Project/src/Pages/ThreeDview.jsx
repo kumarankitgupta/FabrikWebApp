6bf94cab5e90856a43c88fea921d526d253214b0
ANKIT KUMAR GUPTA <93043657+kumarankitgupta@users.noreply.github.com>
2023-04-30 12:53:54 +0530

    Update ThreeDview.jsx
    Minor changes

--- Project/src/Pages/ThreeDview.jsx
+++ Project/src/Pages/ThreeDview.jsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { useGLTF, Stage, OrbitControls,useProgress } from '@react-three/drei';
+import { useGLTF, Stage, OrbitControls, useProgress } from '@react-three/drei';
 import {useParams} from 'react-router-dom';
 import axios from 'axios';
 import LoadingCube from './LoadingCube';
 import './progress.css'
-
 function Model(props) {
-  
   const { scene } = useGLTF('https://fabreactapplication.onrender.com/'+props.param);
   return <primitive object={scene} {...props} />;
 }
+
+
 function Progress({ progress }) {
   return (
     <div className="progress-bar-container">
@@ -20,32 +20,35 @@ function Progress({ progress }) {
     </div>
   );
 }
-
 function ThreeDview() {
-  const {id} = useParams();
-  const [fname,setfname] = useState(undefined);
-  useEffect(()=>{
+  const { id } = useParams();
+  const [fname, setfname] = useState(undefined);
+  useEffect(() => {
     axios.get(`https://fabreactapplication.onrender.com/modelname?id=${id}`)
-    .then((response)=>{
-      console.log(response.data.filename)
-      setfname(response.data.filename)
-    })
-  },[])
+      .then((response) => {
+        console.log(response.data.filename)
+        setfname(response.data.filename)
+      })
+  }, [])
+
+  const { progress } = useProgress();
+  
   return (
     <>
-    {typeof (fname) === 'undefined' ?<LoadingCube/>:
-    <>
-    <Progress progress={progress} />
-    <Canvas dpr={[1, 2]} shadows camera={{ fov: 45 }} style={{ position: 'absolute' }}>
-    <color attach="background" args={['#DDDDDD']} />
-    <OrbitControls enablePan enableZoom enableRotate />
-    <Stage environment="sunset">
-      <Model scale={0.029999} param={fname} />
-    </Stage>
-  </Canvas>
-  </>
-  }
-  </>
+      {typeof (fname) === 'undefined' ?
+        <LoadingCube /> :
+        <>
+          <Progress progress={progress} />
+          <Canvas dpr={[1, 2]} shadows camera={{ fov: 45 }} style={{ position: 'absolute' }}>
+            <color attach="background" args={['#DDDDDD']} />
+            <OrbitControls enablePan enableZoom enableRotate />
+            <Stage environment="sunset">
+              <Model scale={0.029999} param={fname} />
+            </Stage>
+          </Canvas>
+        </>
+      }
+    </>
   );
 }
 
