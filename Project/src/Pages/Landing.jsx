2e6b477e75f55f5e98bfb5c5488479aaaa716a4d
ANKIT KUMAR GUPTA <93043657+kumarankitgupta@users.noreply.github.com>
2023-05-15 15:12:51 +0530

    Update Landing.jsx

--- Project/src/Pages/Landing.jsx
+++ Project/src/Pages/Landing.jsx
@@ -9,7 +9,7 @@ function Landing() {
   const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    axios.get('http://13.232.17.133:3001/getdata')
+    axios.get('https://main--velvety-praline-56c3a3.netlify.app/api/getdata')
       .then(response => {
         setArr(response.data);
       });
