e1fe24543aca6903aaf90df0092235d7c6c25b83
Ankit Kumar Gupta <ankitkrgupta149@gmail.com>
2023-08-27 20:44:51 +0530

    minor changes

--- Project/src/Pages/ThreeDview.jsx
+++ Project/src/Pages/ThreeDview.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { useGLTF, Stage, OrbitControls, useProgress } from '@react-three/drei';
+import { useGLTF, Stage, OrbitControls, useProgress,Environment,Stats } from '@react-three/drei';
 import {useParams} from 'react-router-dom';
 import axios from 'axios';
 import LoadingCube from './LoadingCube';
@@ -10,7 +10,6 @@ function Model(props) {
   const { scene } = useGLTF('https://main--velvety-praline-56c3a3.netlify.app/api/'+props.param);
   return <primitive object={scene} {...props} />;
 }
-
 
 function Progress({ progress }) {
   return (
@@ -41,12 +40,16 @@ function ThreeDview() {
         <LoadingCube /> :
         <>
           <Progress progress={progress} />
-          <Canvas dpr={[1, 2]} shadows camera={{ fov: 45 }} style={{ position: 'absolute' }}>
+          <Canvas dpr={[1, 2]} shadows camera={{ position: [-0.5, 2, 3] }} style={{ position: 'absolute' }}>
+          <Environment
+        files="https://cdn.jsdelivr.net/gh/Sean-Bradley/React-Three-Fiber-Boilerplate@environment/public/img/venice_sunset_1k.hdr"
+        background
+        blur={0.5}
+      />
+      <directionalLight position={[5, 5, 5]} intensity={1} />
             <color attach="background" args={['#DDDDDD']} />
-            <OrbitControls enablePan enableZoom enableRotate />
-            <Stage environment="sunset">
-              <Model scale={0.029999} param={fname} />
-            </Stage>
+            <OrbitControls enablePan enableZoom enableRotate autoRotate />
+            <Model scale={8.89999} param={fname} />
           </Canvas>
         </>
       }
